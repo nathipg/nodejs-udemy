39f2678023fc71b17d29863c453d70118cf3c1b2
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-08-22 13:43:23 -0300

    Using Node Modules System

--- node-server/app.js
+++ node-server/app.js
@@ -1,54 +1,7 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+const routes = require('./routes');
 
-
-  if(method === 'GET') {
-    res.setHeader('Content-Type', 'text/html');
-
-    res.write(`
-      <html>
-        <head>
-          <title>My First Page</title>
-        </head>
-        <body>
-    `);
-  }
-
-  if(url === '/') {
-    res.write(`
-      <form action="/message" method="POST">
-        <input type="text" name="message" />
-        <button type="submit">Submit</button>
-      </form>
-    `);
-  } else if(url === '/message' && method === 'POST') {
-    const body = [];
-
-    req.on('data', chunk => {
-      body.push(chunk);
-    });
-
-    return req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, err => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    });
-  }
-
-  res.write(`
-      </body>
-    </html>
-  `);
-
-  res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
