6ac0e13f60131e5e0aca04c57f64de9d48c69832
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-08-22 12:40:52 -0300

    Response

--- node-server/app.js
+++ node-server/app.js
@@ -2,6 +2,19 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+  
+  res.setHeader('Content-Type', 'text/html');
+  res.write(`
+    <html>
+      <head>
+        <title>My First Page</title>
+      </head>
+      <body>
+        <h1>Hello!</h1>
+      </body>
+    </html>
+  `);
+  res.end();
 });
 
 server.listen(3000);
