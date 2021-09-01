1d4f1a475ee0f170d1ce08614128f93fb3892ab1
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-09-01 19:51:20 -0300

    404 page

--- express-js/app.js
+++ express-js/app.js
@@ -6,9 +6,15 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+app.use('/', (req, res, next) => {
+  res
+    .status(404)
+    .send(`<h1>404 Page not found</h1>`);
+});
 
 app.listen(3000);
