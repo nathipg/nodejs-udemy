a1762aa7a6c9aab501bcbe72a34e5169da124e49
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-09-01 19:49:05 -0300

    Express Router

--- express-js/app.js
+++ express-js/app.js
@@ -3,24 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/add-product', (req, res, next) => {
-  res.send(`
-    <form action="/product" method="POST">
-      <input type="text" name="title" />
-      <button type="submit">Add Product</button>
-    </form>
-  `);
-});
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
