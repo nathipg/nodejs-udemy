d493fb29f04779a2f044a81aa2075b83c760de7a
Nathália Pissuti <nathaliapissuti@gmail.com>
2021-09-03 14:19:07 -0300

    Templating Engines - EJS

--- express-js/app.js
+++ express-js/app.js
@@ -2,16 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs({
-  layoutsDir: 'views/layouts/', // Default
-  defaultLayout: 'main-layout',
-  extname: 'hbs',
-}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views'); // Default
 
 const adminData = require('./routes/admin');
@@ -27,7 +21,8 @@ app.use('/', (req, res, next) => {
   res
     .status(404)
     .render('404', {
-      docTitle: 'Page not found'
+      docTitle: 'Page not found',
+      path: '',
     });
 });
 
