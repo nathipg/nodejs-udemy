80e00d2c84ba2677589bfb73533f5b7cd64c46f1
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-01-11 13:52:14 -0300

    Adding an order

--- app-mongo/models/user.js
+++ app-mongo/models/user.js
@@ -70,14 +70,31 @@ class User {
   }
 
   deleteItemFromCart(productId) {
-    const updatedCartItems = this.cart.items.filter(item => item.productId.toString() !== productId.toString());
+    const updatedCartItems = this.cart.items.filter(
+      item => item.productId.toString() !== productId.toString()
+    );
     const db = getDb();
     return db
       .collection('users')
       .updateOne(
         { _id: new mongodb.ObjectId(this.id) },
-        { $set: { cart: {items: updatedCartItems} } }
+        { $set: { cart: { items: updatedCartItems } } }
       );
+  }
+
+  addOrder() {
+    const db = getDb();
+    return db.collection('orders')
+      .insertOne(this.cart)
+      .then(result => {
+        this.cart = { items: [] };
+        return db
+          .collection('users')
+          .updateOne(
+            { _id: new mongodb.ObjectId(this.id) },
+            { $set: { cart: { items: [] } } }
+          );
+      });
   }
 
   static findById(id) {
