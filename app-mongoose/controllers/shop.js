79645df1a2d9fcae91b0d4ce48ad8f4913f47d4a
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-01-12 14:31:00 -0300

    Getting orders

--- app-mongoose/controllers/shop.js
+++ app-mongoose/controllers/shop.js
@@ -118,8 +118,7 @@ exports.postOrder = async (req, res, next) => {
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({ 'user.userId': req.user._id })
     .then(orders => {
       res.render('shop/orders', {
         path: '/orders',
