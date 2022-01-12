21275f3f4a0ac90abab67af64b6712c5b77ea2d9
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-01-12 13:41:26 -0300

    Shopping cart

--- app-mongoose/models/user.js
+++ app-mongoose/models/user.js
@@ -28,6 +28,31 @@ const userSchema = new Schema({
   },
 });
 
+userSchema.methods.addToCart = function (product) {
+  const cartProductIndex = this.cart.items.findIndex(
+    cp => cp.productId.toString() == product._id.toString()
+  );
+  let newQty = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex > -1) {
+    newQty = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQty;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQty,
+    });
+  }
+
+  const updatedCart = {
+    items: updatedCartItems,
+  };
+
+  this.cart = updatedCart;
+  return this.save();
+};
+
 module.exports = mongoose.model('User', userSchema);
 
 // const mongodb = require('mongodb');
