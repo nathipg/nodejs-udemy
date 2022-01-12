9e1de7dca14db680fadc4c41a27b80589140d193
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-01-12 13:25:45 -0300

    Adding and using an user model

--- app-mongoose/app.js
+++ app-mongoose/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +18,14 @@ const errorRoutes = require('./routes/error'); // Must be the last one
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('616361b6426344613bb0bbc0')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('61defff33b65d0f7354338cc')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -36,6 +36,19 @@ mongoose
     'mongodb+srv://root:root@cluster0.gqup1.mongodb.net/shop?retryWrites=true&w=majority'
   )
   .then(result => {
+    User.findOne().then(user => {
+      if(!user) {
+        const user = new User({
+          name: 'Pissuti',
+          email: 'pissuti@email.com',
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+
     app.listen(3000);
   })
   .catch(err => console.error(err));
