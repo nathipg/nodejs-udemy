6be82b9535fbcdde259653630982b355c13585c2
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-01-12 13:53:31 -0300

    Loading the cart

--- app-mongoose/controllers/shop.js
+++ app-mongoose/controllers/shop.js
@@ -51,11 +51,11 @@ exports.getIndex = (req, res, next) => {
     });
 };
 
-exports.getCart = (req, res, next) => {
-  req.user
-    .getCart()
-    .then(products => {
-      console.log(products)
+exports.getCart = async (req, res, next) => {
+  await req.user
+    .populate('cart.items.productId')
+    .then(user => {
+      const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Cart',
