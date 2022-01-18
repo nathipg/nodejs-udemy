d7f26cadf853e78a54d2df0de643c989e4f9d1f6
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-01-17 21:53:37 -0300

    Manipulating cookies

--- app-mongoose/controllers/auth.js
+++ app-mongoose/controllers/auth.js
@@ -1,6 +1,5 @@
 exports.getLogin = (req, res, next) => {
-  console.log(req.get('Cookie'));
-  const isLoggedIn = req.get('Cookie').split(';')[0].trim().split('=')[1];
+  const isLoggedIn = req.get('Cookie').split(';')[0].trim().split('=')[1] === 'true';
 
   res.render('auth/login', {
     path: '/login',
