f7ccc0d64767b8695a300a3cae6c422f74c094f9
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-01-19 21:14:41 -0300

    Validation

--- app-mongoose/controllers/auth.js
+++ app-mongoose/controllers/auth.js
@@ -2,6 +2,7 @@ const crypto = require('crypto');
 
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
+const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 
@@ -84,6 +85,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+
+  if(!errors.isEmpty()) {
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array(),
+    });
+  }
 
   User.findOne({ email: email })
     .then(userDoc => {
