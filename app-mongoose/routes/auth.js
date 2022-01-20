47ee0f699f63bc7da13cbb0514e3aeb10ed3388a
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-01-19 21:17:11 -0300

    Error messages

--- app-mongoose/routes/auth.js
+++ app-mongoose/routes/auth.js
@@ -15,7 +15,11 @@ router.get('/reset/:token', authController.getNewPassword);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', check('email').isEmail(), authController.postSignup);
+router.post(
+  '/signup',
+  check('email').isEmail().withMessage('Invalid email'),
+  authController.postSignup
+);
 
 router.post('/logout', authController.postLogout);
 
