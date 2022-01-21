cbc8df8579568fc656570e7a58a83d2fbee37cf0
Nathália Pissuti <nathaliapissuti@gmail.com>
2022-01-20 21:57:32 -0300

    Error pages

--- app-mongoose/controllers/error.js
+++ app-mongoose/controllers/error.js
@@ -1,6 +1,13 @@
 exports.get404 = (req, res, next) => {
   res.status(404).render('404', {
     pageTitle: 'Page not found',
-    path: '',
+    path: '/404',
+  });
+};
+
+exports.get500 = (req, res, next) => {
+  res.status(500).render('500', {
+    pageTitle: 'Some error occured',
+    path: '/500',
   });
 };
--- app-mongoose/routes/error.js
+++ app-mongoose/routes/error.js
@@ -4,6 +4,8 @@ const errorController = require('../controllers/error');
 
 const router = express.Router();
 
+router.use('/500', errorController.get500);
+
 router.use(errorController.get404);
 
 module.exports = router;
